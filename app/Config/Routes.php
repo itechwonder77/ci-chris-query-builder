615fd0714d1ef78100533c647014579e3289bd74
padoc-cloud <17752709+padoc-cloud@users.noreply.github.com>
2025-04-25 11:52:00 -0400

    feat: Created EventsModal and relevant file according to MVC structure

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,7 +2,12 @@
 
 use CodeIgniter\Router\RouteCollection;
 
+use App\Controllers\EventsController;
+
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('events', 'EventsController::index');
+$routes->post('api/v1/events/create', 'EventsController::create');
